d09c5f1f7b9f99afd14a70c4448b9e4548723efa
xianengqi <xianengqi@gmail.com>
2020-10-20 23:09:35 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 夏能啟 on 2020/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Sample_DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by 夏能啟 on 2020/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Sample_DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
