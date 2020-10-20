1e0b4a2394a93e31fd3c9c2ad7f1b0ddb0289531
xianengqi <xianengqi@gmail.com>
2020-10-21 01:19:09 +0800

    feat: Add tabBar in the iPad or iOS

--- Shared/Sample_DesignCodeCourseApp.swift
+++ Shared/Sample_DesignCodeCourseApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Sample_DesignCodeCourseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            SideBar()
         }
     }
 }
