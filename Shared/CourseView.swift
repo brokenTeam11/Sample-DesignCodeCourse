270129eb2d49062615ba727b43e0c2e89815644f
xianengqi <xianengqi@gmail.com>
2020-10-21 00:26:26 +0800

    feat: Add SF in the TabBar

---
+++ Shared/CourseView.swift
@@ -0,0 +1,23 @@
+//
+//  CourseView.swift
+//  Sample-DesignCodeCourse
+//
+//  Created by 夏能啟 on 2020/10/21.
+//
+
+import SwiftUI
+
+struct CourseView: View {
+    var body: some View {
+        List(0 ..< 20) { item in
+            CourseRow()
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct CourseView_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseView()
+    }
+}
