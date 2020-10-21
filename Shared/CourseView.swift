0ac32a88d669dad6f3d6f7c557fb113435654a54
xianengqi <xianengqi@gmail.com>
2020-10-22 01:23:19 +0800

    feat: Visual Effect Blur for macOS

--- Shared/CourseView.swift
+++ Shared/CourseView.swift
@@ -15,56 +15,80 @@ struct CourseView: View {
 
     var body: some View {
         ZStack {
-            ScrollView {
-                // layout布局
-                LazyVGrid(
-                    columns: [
-                        GridItem(.adaptive(minimum: 160), spacing: 16)
-                    ],
-                    spacing: 16)
-                {
-                    ForEach(courses) { item in
-                        VStack {
-                            CourseItem(course: item)
-                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                                .frame(height: 200)
-                                .onTapGesture {
-                                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) { // 建议使用`withAnimation`，来添加点击动效，有更好的过渡，不会造成拖拽拖影
-                                        show.toggle()
-                                        selectedItem = item
-                                        isDisabled = true
-                                    }
-                                }
-                                .disabled(isDisabled)
-                        }
-                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
-                    }
-                }
-                .padding(16)
-                .frame(maxWidth: .infinity)
-            }
-            .zIndex(1)
-            if selectedItem != nil {
-                ZStack(alignment: .topTrailing) {
-                    CourseDetail(course: selectedItem!, namespace: namespace)
-                    
-                    CloseButton()
-                        .padding(.trailing, 16)
-                        .onTapGesture {
-                            withAnimation(.spring()) { // 建议使用`withAnimation`，来添加点击动效，有更好的过渡，不会造成拖拽拖影
-                                show.toggle()
-                                selectedItem = nil
-                                // 设置动画延迟, 延迟时间结束才能运行此代码
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                    isDisabled = false
+            #if os(iOS)
+            content
+                .navigationBarHidden(true)
+            fullContent
+                .background(VisualEffectBlur(blurStyle: .systemMaterial).edgesIgnoringSafeArea(.all)) // `VisualEffectBlur` 是Apple团队开源的用于处理背景模糊
+            #else
+            content
+            fullContent
+                .background(VisualEffectBlur().edgesIgnoringSafeArea(.all)) // `VisualEffectBlur` 是Apple团队开源的用于处理背景模糊
+            #endif
+        }
+        .navigationTitle("Courses")
+    }
+    // 用于适配iOS和MacOS
+    var content: some View {
+        ScrollView {
+            // layout布局
+            LazyVGrid(
+                columns: [
+                    GridItem(.adaptive(minimum: 160), spacing: 16)
+                ],
+                spacing: 16)
+            {
+                ForEach(courses) { item in
+                    VStack {
+                        CourseItem(course: item)
+                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame(height: 200)
+                            .onTapGesture {
+                                withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) { // 建议使用`withAnimation`，来添加点击动效，有更好的过渡，不会造成拖拽拖影
+                                    show.toggle()
+                                    selectedItem = item
+                                    isDisabled = true
                                 }
                             }
-                        }
+                            .disabled(isDisabled)
+                    }
+                    .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
                 }
-                .zIndex(2)
             }
+            .padding(16)
+            .frame(maxWidth: .infinity)
+        }
+        .zIndex(1)
+
+    }
+    
+    // 用于适配iOS和MacOS
+    @ViewBuilder
+    var fullContent: some View {
+        if selectedItem != nil {
+            ZStack(alignment: .topTrailing) {
+                CourseDetail(course: selectedItem!, namespace: namespace)
+                
+                CloseButton()
+                    .padding(16)
+                    .onTapGesture {
+                        withAnimation(.spring()) { // 建议使用`withAnimation`，来添加点击动效，有更好的过渡，不会造成拖拽拖影
+                            show.toggle()
+                            selectedItem = nil
+                            // 设置动画延迟, 延迟时间结束才能运行此代码
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                isDisabled = false
+                            }
+                        }
+                    }
+            }
+            .zIndex(2)
+            .frame(maxWidth: 712)
+            .frame(maxWidth: .infinity)
+            
         }
     }
+    
 }
 
 struct CourseView_Previews: PreviewProvider {
